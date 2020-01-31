bfa96948e8b50df75bfb25258ef02196a3465ed4
Patryk Obara <dreamer.tan@gmail.com>
2020-01-31 23:55:51 +0100

    WIP Repackage to icns

--- pack.sh
+++ pack.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 set -x
+
+# Prepare icon file
+pushd contrib/icons/
+mkdir dosbox-staging.iconset
+cp              dosbox-staging-16.png         dosbox-staging.iconset/icon_16x16.png
+sips -z 32 32   dosbox-staging-1024.png --out dosbox-staging.iconset/icon_16x16@2x.png
+sips -z 32 32   dosbox-staging-1024.png --out dosbox-staging.iconset/icon_32x32.png
+sips -z 64 64   dosbox-staging-1024.png --out dosbox-staging.iconset/icon_32x32@2x.png
+sips -z 128 128 dosbox-staging-1024.png --out dosbox-staging.iconset/icon_128x128.png
+sips -z 256 256 dosbox-staging-1024.png --out dosbox-staging.iconset/icon_128x128@2x.png
+sips -z 256 256 dosbox-staging-1024.png --out dosbox-staging.iconset/icon_256x256.png
+sips -z 512 512 dosbox-staging-1024.png --out dosbox-staging.iconset/icon_256x256@2x.png
+sips -z 512 512 dosbox-staging-1024.png --out dosbox-staging.iconset/icon_512x512.png
+cp              dosbox-staging-1024.png       dosbox-staging.iconset/icon_512x512@2x.png
+iconutil -c icns dosbox-staging.iconset
+popd
+
 # Prepare content
 install -d dist/dosbox-staging.app/Contents/MacOS/
 install -d dist/dosbox-staging.app/Contents/Resources/
