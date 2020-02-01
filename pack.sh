8c0464279b9504a052731df6e28d578ce58edfa4
Patryk Obara <dreamer.tan@gmail.com>
2020-02-01 01:55:09 +0100

    WIP fix pack.sh

--- pack.sh
+++ pack.sh
@@ -24,14 +24,14 @@ install -d dist/dosbox-staging.app/Contents/SharedSupport/
 
 # install -d dosbox-staging.app/Contents/_CodeSignature/ # ?
 
-install        src/dosbox                       dist/dosbox-staging.app/Contents/MacOS/dosbox
-install -m 644 contrib/macos/Info.plist         dist/dosbox-staging.app/Contents/Info.plist
-install -m 644 contrib/macos/PkgInfo            dist/dosbox-staging.app/Contents/PkgInfo
-install -m 644 contrib/icon/dosbox-staging.icns dist/dosbox-staging.app/Contents/Resources/dosbox-staging.icns
-install -m 644 docs/README.template             dist/dosbox-staging.app/Contents/SharedSupport/README
-install -m 644 COPYING                          dist/dosbox-staging.app/Contents/SharedSupport/COPYING
-install -m 644 README                           dist/dosbox-staging.app/Contents/SharedSupport/manual.txt
-install -m 644 docs/README.video                dist/dosbox-staging.app/Contents/SharedSupport/video.txt
+install        src/dosbox                        dist/dosbox-staging.app/Contents/MacOS/dosbox
+install -m 644 contrib/macos/Info.plist          dist/dosbox-staging.app/Contents/Info.plist
+install -m 644 contrib/macos/PkgInfo             dist/dosbox-staging.app/Contents/PkgInfo
+install -m 644 contrib/icons/dosbox-staging.icns dist/dosbox-staging.app/Contents/Resources/dosbox-staging.icns
+install -m 644 docs/README.template              dist/dosbox-staging.app/Contents/SharedSupport/README
+install -m 644 COPYING                           dist/dosbox-staging.app/Contents/SharedSupport/COPYING
+install -m 644 README                            dist/dosbox-staging.app/Contents/SharedSupport/manual.txt
+install -m 644 docs/README.video                 dist/dosbox-staging.app/Contents/SharedSupport/video.txt
 
 ln -s /Applications dist/
 
